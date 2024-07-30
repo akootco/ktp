9731c1bbeedc5fd5e48f95fc6650a950f7365ccf
akoot <akoot@akoot.me>
2024-07-30 16:29:06 -0400

    Removed the onEnable message which was retained from the obvious template (commedore)

--- src/main/kotlin/co/akoot/plugins/ktp/KTP.kt
+++ src/main/kotlin/co/akoot/plugins/ktp/KTP.kt
@@ -3,12 +3,6 @@ package co.akoot.plugins.ktp
 import org.bukkit.plugin.java.JavaPlugin
 
 class KTP : JavaPlugin() {
-
-    override fun onEnable() {
-        logger.info("We are eating good tonight!")
-    }
-
-    override fun onDisable() {
-        // Plugin shutdown logic
-    }
+    override fun onEnable() {}
+    override fun onDisable() {}
 }
